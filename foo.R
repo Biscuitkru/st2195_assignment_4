47bb63da8dd2874b0eff5dca7fe266650f0ed0c1
Victor Min <90329393+Biscuitkru@users.noreply.github.com>
2021-11-18 20:53:59 +0800

    Update foo.R

--- foo.R
+++ foo.R
@@ -3,23 +3,19 @@ r <- 2
 
 # Function to compute the volume of a sphere with radius r
 volume <- function(r, pi = 3.14) {
-  sphere_vol <- 3/4*pi*r^2
+  sphere_vol <- 4/3*pi*r^3
   print(paste("The volume of a sphere with radius", r , "is", sphere_vol))
 }
 volume(2)
 
 # Function to compute the volumes of the spheres with radius r,r^2 and r^3
 volume_vector <- function(r){
-  r <- 22
-  k <- numeric(3)
+  volumes = c()
   for(i in 1:3){
-    k[i] = r^i
-  }
-    {
-    volume(k)
+  volumes = c(volumes, volume(r^i))
   }
 }
 
 # Run volume_vector(r) and print the volumes of the spheres with radius r,r^2 and r^3
-volume_vector(k)
+volume_vector(r)
 
